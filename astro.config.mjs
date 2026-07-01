e91be7eaf0ec8ace705886b626ca1c1b85a9f2c6
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2026-07-02 01:22:49 +0300

    feat: настроить деплой на GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://yaleksandr89.github.io',
   integrations: [icon()],
   output: 'static',
 });
